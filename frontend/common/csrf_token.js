549ca02825ab91f0f4ee1873b18a44b81d664a90
Radoslaw Hryniewicki <radek.hryn@gmail.com>
2019-08-01 13:39:33 +0200

    js-cookie-get-csrftoken

---
+++ frontend/common/csrf_token.js
@@ -0,0 +1,25 @@
+// https://docs.djangoproject.com/en/2.2/ref/csrf/
+
+// funkcja pozwalająca na pobranie tokenu z cookie
+
+// function getCookie(name) {
+//     var cookieValue = null;
+//     if (document.cookie && document.cookie !== '') {
+//         var cookies = document.cookie.split(';');
+//         for (var i = 0; i < cookies.length; i++) {
+//             var cookie = cookies[i].trim();
+//             // Does this cookie string begin with the name we want?
+//             if (cookie.substring(0, name.length + 1) === (name + '=')) {
+//                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+//                 break;
+//             }
+//         }
+//     }
+//     return cookieValue;
+// }
+// var CSRF_TOKEN = getCookie('csrftoken');
+
+// export { CSRF_TOKEN };
+
+var CSRF_TOKEN = Cookies.get('csrftoken');
+export { CSRF_TOKEN };
