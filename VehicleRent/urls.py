674f834ed94a500bb22d2f037a020639ac42ecc9
Radoslaw Hryniewicki <radek.hryn@gmail.com>
2019-08-01 11:17:35 +0200

    import_repath

--- VehicleRent/urls.py
+++ VehicleRent/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from users.forms import CustomUserForm
 from core.views import IndexTemplateView
 from django_registration.backends.one_step.views import RegistrationView
