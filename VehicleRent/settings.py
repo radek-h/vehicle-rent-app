c85f4d4bd80ea809aa6b36625fddf99d49280ef4
Radoslaw <radek.hryn@gmail.com>
2019-08-12 12:10:12 +0200

    changed_media_url_root

--- VehicleRent/settings.py
+++ VehicleRent/settings.py
@@ -133,8 +133,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-MEDIA_URL = '/frontend/src/assets/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'frontend/src/assets')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL = "accounts/login/" # RH #
 LOGIN_REDIRECT_URL = "/"
