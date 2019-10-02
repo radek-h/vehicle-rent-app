6facadc4b23a938d1d00e467f6699f415984af9b
Radoslaw Hryniewicki <radek.hryn@gmail.com>
2019-10-02 21:51:44 +0200

    deleted static folder

--- VehicleRent/settings.py
+++ VehicleRent/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-LOGIN_URL = "accounts/login/" # RH #
+LOGIN_URL = "accounts/login/" # RH
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
